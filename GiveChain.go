60c9999cb73d69a9f372b3a7c07bcf58860a1f6c
xxxmandoxxx <pthurner@gtlaw.com.au>
2016-05-26 21:12:28 +1000

    Update GiveChain.go

--- GiveChain.go
+++ GiveChain.go
@@ -23,6 +23,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 
@@ -38,7 +39,7 @@ type SimpleChaincode struct {
 type Donation struct {
 	Id   		string  `json:"id"`
 	Owner  		string  `json:"owner"`
-	Amount		int 	`json:"amount"`
+	Amount		int64 	`json:"amount"`
 	ProjectID	string 	`json:"projectID"`
 	Transactions []Transaction `json:"transactions"`
 }
@@ -47,9 +48,11 @@ type Transaction struct {
 	Id   		string  `json:"id"`
 	tDate		string	`json:"tdate"`
 	Destination  	string  `json:"destination"`
-	Amount		int 	`json:"amount"`
+	Amount		int64 	`json:"amount"`
 	ProjectID	string 	`json:"projectID"`
 	TType 		string   `json:"ttype"`
+	Details 	string	`json:"details"`
+	SupplierID	string	`json:"SupplierID"`
 }
 
 type AllDonations struct{
@@ -60,6 +63,19 @@ type AllDonationsDetails struct{
 	Donations []Donation `json:"donations"`
 }
 
+type ProjectAmount struct {
+	Name	string	`json:"name"`
+	Amount	int64	`json:"amount"`
+}
+
+type Supplier struct {
+	ID	string	`json:"id"`
+	Transactions []Transaction `json:"transactions"`
+}
+
+type AllSuppliers struct{
+	Suppliers []string `json:"suppliers"`
+}
 // ============================================================================================================================
 // Init 
 // ============================================================================================================================
@@ -70,6 +86,13 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	var donations AllDonations
 	jsonAsBytes, _ := json.Marshal(donations)
 	err = stub.PutState("allDonations", jsonAsBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	var suppliers AllSuppliers
+	supAsBytes, _ := json.Marshal(suppliers)
+	err = stub.PutState("allSuppliers", supAsBytes)
 	if err != nil {
 		return nil, err
 	}
@@ -119,6 +142,7 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	if function == "getAllDonationsByUserId" { return t.getAllDonationsByUserId(stub, args[0]) }
 	if function == "getDonation" { return t.getDonation(stub, args[0]) }
 	if function == "getTransactions" { return t.getTransactions(stub, args[0]) }
+	if function == "getAmount" { return t.getAmount(stub, args[0]) }
 
 	return nil, nil										
 }
@@ -155,8 +179,6 @@ func (t *SimpleChaincode) getAllDonationsByUserId(stub *shim.ChaincodeStub, user
 
 		if(sd.Owner == userID) {
 			rad.Donations = append(rad.Donations, sd.Id);
-		} else {
-			rad.Donations = append(rad.Donations, "TESTING")
 		}
 
 	}
@@ -189,9 +211,9 @@ func (t *SimpleChaincode) createDonation(stub *shim.ChaincodeStub, args []string
 	var err error
 	fmt.Println("Running create Donation")
 
-	if len(args) != 6 {
-		fmt.Println("Incorrect number of arguments. Expecting 6")
-		return nil, errors.New("Incorrect number of arguments. Expecting 6")
+	if len(args) != 5 {
+		fmt.Println("Incorrect number of arguments. Expecting 5")
+		return nil, errors.New("Incorrect number of arguments. Expecting 5")
 	}
 
 
@@ -205,7 +227,7 @@ func (t *SimpleChaincode) createDonation(stub *shim.ChaincodeStub, args []string
 	tx.ProjectID		= args[1]
 	tx.TType 		= "CREATE"
 	intAmount, err := strconv.Atoi(args[0])
-	tx.Amount		= intAmount
+	tx.Amount		= int64(intAmount)
 	tx.tDate		= args[4]
 	tx.Id			= args[3] + "T1"
 
@@ -277,6 +299,230 @@ func (t *SimpleChaincode) getTransactions(stub *shim.ChaincodeStub, donationID s
 
 }
 
+func (t *SimpleChaincode) getAmount(stub *shim.ChaincodeStub, projectID string)([]byte, error){
+
+	fmt.Println("Start find all Donations ")
+	fmt.Println("Looking for all Donations with project id " + projectID);
+
+	//get the AllBatches index
+	allDonsBytes, err := stub.GetState("allDonations")
+	if err != nil {
+		return nil, errors.New("Failed to get all Donations")
+	}
+
+	var res AllDonations
+	err = json.Unmarshal(allDonsBytes, &res)
+	if err != nil {
+		return nil, errors.New("Failed to Unmarshal all Donations")
+	}
+
+	var pAmount ProjectAmount
+	pAmount.Name = projectID
+
+
+	for i := range res.Donations{
+
+		sdASBytes, err := stub.GetState(res.Donations[i])
+		if err != nil {
+			return nil, errors.New("Failed to get Donation")
+		}
+		var sd Donation
+		json.Unmarshal(sdASBytes, &sd)
+
+		if(sd.ProjectID == projectID) {
+			for x := range sd.Transactions{
+				var tx Transaction
+				tx = sd.Transactions[x]
+				pAmount.Amount = pAmount.Amount + tx.Amount
+			}
+
+		}
+
+	}
+
+	paAsBytes, _ := json.Marshal(pAmount)
+
+	return paAsBytes, nil
+
+}
+
+
+
+func (t *SimpleChaincode) transfer(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	if len(args) != 3 {
+		fmt.Println("Incorrect number of arguments. Expecting ")
+		return nil, errors.New("Incorrect number of arguments. Expecting 5")
+	}
+
+	var supplierID string = args[0]
+	var projectID string = args[1]
+	intAmount, err := strconv.Atoi(args[2])
+	if err != nil {
+		return nil, errors.New("Failed to get Amount")
+	}
+	var amount int64 = int64(intAmount)
+	var originalAmount int64 = int64(intAmount)
+	pAmountAsByte , err := t.getAmount(stub,projectID)
+
+	var pAmount ProjectAmount
+	json.Unmarshal(pAmountAsByte, &pAmount)
+
+	if pAmount.Amount > amount {
+
+		//CHECK IF SUPPLIER EXISTS
+		supAsByte , err := stub.GetState(supplierID)
+		if err != nil {
+			//CREATE SUPPLIER AND ADD TO LEDGER AND ALLSUPPLIERS STATE
+			var supplier Supplier
+			supplier.ID = supplierID
+
+			sAsBytes, _ := json.Marshal(supplier)
+			err = stub.PutState(supplier.ID, sAsBytes)
+			if err != nil {
+				return nil, err
+			}
+
+			allSUsBytes, err := stub.GetState("allSuppliers")
+			if err != nil {
+				return nil, errors.New("Failed to get all Suppliers")
+			}
+			var allsup AllSuppliers
+			err = json.Unmarshal(allSUsBytes, &allsup)
+			if err != nil {
+				return nil, errors.New("Failed to Unmarshal all Suppliers")
+			}
+			allsup.Suppliers = append(allsup.Suppliers,supplier.ID)
+
+			allSupAsBytes, _ := json.Marshal(allsup)
+			err = stub.PutState("allSuppliers", allSupAsBytes)
+			if err != nil {
+				return nil, err
+				var ttt Supplier
+				json.Unmarshal(supAsByte, &ttt)
+			}
+		}
+
+		newsupAsByte, err := stub.GetState(supplierID)
+		if err != nil {
+			return nil, err
+		}
+
+		var supplier Supplier
+		json.Unmarshal(newsupAsByte, &supplier)
+
+
+
+		allDonsBytes, err := stub.GetState("allDonations")
+		if err != nil {
+			return nil, errors.New("Failed to get all Donations")
+		}
+
+		var res AllDonations
+		err = json.Unmarshal(allDonsBytes, &res)
+		if err != nil {
+			return nil, errors.New("Failed to Unmarshal all Donations")
+		}
+
+		for i := range res.Donations{
+
+			sdASBytes, err := stub.GetState(res.Donations[i])
+			if err != nil {
+				return nil, errors.New("Failed to get Donation")
+			}
+			var sd Donation
+			json.Unmarshal(sdASBytes, &sd)
+
+			if(sd.ProjectID == projectID) {
+				var dAmount int64 = 0
+				var tCount int
+				for x := range sd.Transactions{
+					var tx Transaction
+					tx = sd.Transactions[x]
+					dAmount = dAmount + tx.Amount
+					tCount = x
+				}
+
+				tCount = tCount + 1
+
+				if dAmount < amount {
+					var tempamount int64
+					tempamount = amount - dAmount
+					amount = tempamount
+					var tx Transaction
+					tx.ProjectID		= projectID
+					tx.TType 		= "TRANSFER"
+					tx.Amount		= -dAmount
+					tTime 			:= time.Now()
+					tx.tDate		= tTime.Format("2006-01-02 15:04:05")
+					tx.Id			= sd.Id + "T" + strconv.Itoa(tCount)
+					tx.SupplierID		= supplier.ID
+
+					sd.Transactions = append(sd.Transactions, tx)
+
+					//Commit batch to ledger
+					fmt.Println("createDonation Commit Donation To Ledger");
+					doAsBytes, _ := json.Marshal(sd)
+					err = stub.PutState(sd.Id, doAsBytes)
+					if err != nil {
+						return nil, err
+					}
+
+					amount = amount - tempamount
+				} else {
+
+					var tx Transaction
+					tx.ProjectID		= projectID
+					tx.TType 		= "TRANSFER"
+					tx.Amount		= -amount
+					tTime 			:= time.Now()
+					tx.tDate		= tTime.Format("2006-01-02 15:04:05")
+					tx.Id			= sd.Id + "T" + strconv.Itoa(tCount)
+					tx.SupplierID		= supplier.ID
+
+					sd.Transactions = append(sd.Transactions, tx)
+
+
+					fmt.Println("createDonation Commit Donation To Ledger");
+					doAsBytes, _ := json.Marshal(sd)
+					err = stub.PutState(sd.Id, doAsBytes)
+					if err != nil {
+						return nil, err
+					}
+					amount = amount - amount
+				}
+
+				if amount == 0 {
+					var sTx Transaction
+					sTx.ProjectID		= projectID
+					sTx.TType 		= "TRANSFER"
+					sTx.Amount		= originalAmount
+					tTime 			:= time.Now()
+					sTx.tDate		= tTime.Format("2006-01-02 15:04:05")
+					sTx.Id			= sd.Id + "T" + strconv.Itoa(tCount) + "S"
+					sTx.SupplierID		= supplier.ID
+
+					supplier.Transactions = append(supplier.Transactions, sTx)
+
+					fmt.Println("createDonation Commit Donation To Ledger");
+					doAsBytes, _ := json.Marshal(supplier)
+					err = stub.PutState(supplier.ID, doAsBytes)
+					if err != nil {
+						return nil, err
+					}
+					return nil, nil
+				}
+
+			}
+
+		}
+
+
+	}
+
+return nil, nil
+
+}
 
 
 func main() {
